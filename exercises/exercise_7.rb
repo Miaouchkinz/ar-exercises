c2bf65128e90654d018afd0cdcd7e5456de9925f
Gab Richard <g.elodierichard@gmail.com>
2020-03-02 17:12:00 -0500

    asks a user to input a store name and create a new entry to table

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# 2. Ask the user for a store name (store it in a variable)
+puts "Enter Store name"
+name = gets.chomp
+
+# 3. Attempt to create a store with the inputted name but leave
+#    out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+store = Store.create(name: name)
+
+# 4. Display the error messages provided back from ActiveRecord
+#    to the user (one on each line) after you attempt to save/create
+#    the record
+begin
+  store.save
+rescue ActiveRecord::RecordInvalid => invalid
+  puts invalid.record.errors
+end
