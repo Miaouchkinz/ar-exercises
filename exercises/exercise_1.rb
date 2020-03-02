6583965915bd673e47bd04f3bf0c2bca437e9585
Gab Richard <g.elodierichard@gmail.com>
2020-03-02 13:56:48 -0500

    added 3 new stores to the stores db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+
+Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+
+# Output (puts) the number of the stores using ActiveRecord's
+# count method, to ensure that there are three stores in the database.
+puts Store.count
